6034c4c3c32057517f69cad8d28b1f193f16607d
GCantral <77795386+GCantral@users.noreply.github.com>
2022-04-10 20:56:32 -0400

    Completed Auto Encoder
     - Completed Auto encoder model and added auto testing of images

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
 import traceback
 import argparse
+import io
+import matplotlib.pyplot as plt
 from functools import partial
 import tensorflow as tf
 from tensorflow.keras import Model, optimizers, losses, metrics
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Flatten, Activation, \
-    Conv2D, MaxPooling2D, Lambda, Input
+    Conv2D, MaxPooling2D, Lambda, Input, Conv2DTranspose, Reshape
+from tensorflow.keras.losses import mse
 from tensorflow.keras.callbacks import TensorBoard
 import keras_tuner as kt
+from tensorflow.keras import backend as K
 from src import *
 
 
@@ -38,6 +42,38 @@ def get_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
+def plot_to_image(figure):
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(figure)
+    buf.seek(0)
+
+    face = tf.image.decode_png(buf.getvalue(), channels=4)
+    face = tf.expand_dims(face, 0)
+
+    return face
+
+
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian.
+
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+
+    # Returns
+        z (tensor): sampled latent vector
+    """
+    # Extract mean and log of variance
+    z_mean, z_log_var = args
+    # get batch size and length of vector (size of latent space)
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    # Return sampled number (need to raise var to correct power)
+    return z_mean + K.exp(z_log_var) * epsilon
+
 
 def build_model_Dense(input_shape: Tuple[int, int], n_classes: int, lr: float = 0.001) -> Model:
     """ Build a feed-forward Dense neural network"""
@@ -59,7 +95,7 @@ def build_model_task_2_conv(input_shape: Tuple[int, int], n_classes: int, lr: fl
     # input_shape = list(input_shape)
     # input_shape.append(1)
     # Add the layers
-    model.add(Conv2D(filters=40, kernel_size=15, activation='relu', input_shape=input_shape))
+    model.add(Conv2D(filters=40, kernel_size=3, activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
@@ -98,22 +134,44 @@ def tune_model_task_3_conv(hp, input_shape: Tuple[int, int], n_classes: int,
 
 
 def build_model_task_5_auto(input_shape: Tuple[int, int], n_classes: int, lr: float = 0.001) -> Model:
-    """ Build a feed-forward conv neural network"""
+    """ Build an Auto Encoder"""
 
+    # Build Encoder
     inputs = Input(shape=input_shape, name='encoder_input')
-    x = Dense(intermediate_dim, activation='relu', name="encoder_hidden_layer")(inputs)
-    z_mean = Dense(latent_dim, name='z_mean')(x)
-    z_log_var = Dense(latent_dim, name='z_log_var')(x)
-
+    cov_1 = Conv2D(filters=20, kernel_size=5, activation='relu')(inputs)
+    cov_2 = Conv2D(filters=20, kernel_size=5, activation='relu')(cov_1)
+    cov_3 = Conv2D(filters=20, kernel_size=5, activation='relu')(cov_2)
+    cov_4 = Conv2D(filters=20, kernel_size=5, activation='relu')(cov_3)
+    flat = Flatten()(cov_4)
+    z_mean = Dense(15, name='z_mean')(flat)
+    z_log_var = Dense(15, name='z_log_var')(flat)
     # use reparameterization trick to push the sampling out as input
     z = Lambda(sampling, name='z')([z_mean, z_log_var])
-
     # instantiate encoder model
-    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder_output')
-    encoder.summary()
+    encoder = Model(inputs, [cov_1, cov_2, cov_3,cov_4, z_mean, z_log_var, z], name='encoder_output')
+    # build decoder model
+    latent_inputs = Input(shape=(15,), name='z_sampling')
+    x_out = Dense(5120, activation='relu', name="decoder_hidden_layer")(latent_inputs)
+    resh = Reshape((16,16,20))(x_out)
+    covT_1 = Conv2DTranspose(filters=20, kernel_size=5, activation='relu')(resh)
+    covT_2 = Conv2DTranspose(filters=20, kernel_size=5, activation='relu')(covT_1)
+    covT_3 = Conv2DTranspose(filters=20, kernel_size=5, activation='relu')(covT_2)
+    covT_4 = Conv2DTranspose(filters=1, kernel_size=5, activation='relu')(covT_3)
+    decoder = Model(latent_inputs, [x_out,resh,covT_1,covT_2,covT_3,covT_4], name='decoder_output')
+    decoder.compile(optimizer='adam')
+    outputs = decoder(encoder(inputs)[6])
+    model = Model(inputs, outputs, name='vae_mlp')
 
-
-    return model
+    reconstruction_loss = mse(inputs, outputs[5])
+    reconstruction_loss *= 1
+    reconstruction_loss = K.mean(reconstruction_loss)
+    kl_loss = K.exp(z_log_var) + K.square(z_mean) - z_log_var - 1
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= 0.001
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    model.add_loss(vae_loss)
+    model.compile(optimizer='adam')
+    return model, decoder
 
 
 def main():
@@ -121,9 +179,9 @@ def main():
     """
 
     # --- Hyper parameters --- #
-    epochs = 10
-    batch_size = 64
-    tuning_image_num = 1000
+    epochs = 50
+    batch_size = 32
+    tuning_image_num = 5000
     tuningEpochs = 20
 
 
@@ -173,8 +231,11 @@ def main():
     if args.task == 1:
         # Flatten the images
         images_train = np.array([image.flatten() for image in images_train])
-    elif args.task in (2, 3, 4, 5):
+    elif args.task in (2, 3, 4):
         images_train = images_train.reshape(*images_train.shape, 1)
+    elif args.task ==5:
+        images_train = images_train.reshape(*images_train.shape, 1)
+        encoded_train_labels = images_train
     # Build the model
 
     # Training Model
@@ -219,6 +280,11 @@ def main():
                                       input_shape=images_train.shape[1:],
                                       n_classes=encoded_train_labels.shape[1],
                                       lr=lr)
+    elif(args.task ==5):
+        model, decoder = build_model(input_shape=images_train.shape[1:],
+                            n_classes=encoded_train_labels.shape[1],
+                            lr=lr)
+        print(model.summary())
     else:
         model = build_model(input_shape=images_train.shape[1:],
                             n_classes=encoded_train_labels.shape[1],
@@ -243,14 +309,49 @@ def main():
             validation_split=validation_set_perc,
             callbacks=callbacks)
 
+
+
+    if(args.task==5):
+        file_writer = tf.summary.create_file_writer(log_folder)
+        figure = plt.figure(figsize=(12, 8))
+
+        predicted = model.predict(images_train[:5])
+        for i in range(5):
+            image = images_train[i].reshape(32,32)
+            plt.subplot(2, 5, i + 1)
+            plt.grid(False)
+            plt.imshow(image)
+            plt.subplot(2,5,i+6)
+            plt.grid(False)
+            plt.imshow(predicted[5][i])
+        with file_writer.as_default():
+            tf.summary.image("Image->Image", plot_to_image(figure), step=0)
+
+
+        figure = plt.figure(figsize=(12, 8))
+        randomInput = np.random.rand(10,15,1)
+        randPredict = decoder.predict(randomInput)
+        for i in range(10):
+            image = images_train[i].reshape(32,32)
+            plt.subplot(2, 5, i + 1)
+            plt.grid(False)
+            plt.imshow(randPredict[5][i])
+
+        with file_writer.as_default():
+            tf.summary.image("Random->Image", plot_to_image(figure), step=0)
+
+
     # Run "tensorboard --logdir logs/fit" in terminal and open http://localhost:6006/
 
     # --- Evaluation --- #
     # Flatten the images
     if args.task == 1:
         images_test = np.array([image.flatten() for image in images_test])
-    elif args.task in (2, 3, 5):
+    elif args.task in (2, 3):
         images_train = images_train.reshape(*images_train.shape, 1)
+    elif args.task ==5:
+        images_train = images_train.reshape(*images_train.shape, 1)
+        encoded_train_labels = images_train
     # Evaluate the model
     # model.evaluate(images_test, encoded_test_labels)
 
